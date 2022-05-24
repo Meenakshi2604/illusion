013f34604681cd0dc9aec8fb75a6d91b62b6ba25
Meenakshi Nair <meenakshin@firstbytedigital.com>
2022-05-24 15:38:14 +0530

    First implementation of stt page

--- lib/screens/speech_to_text/stt_page.dart
+++ lib/screens/speech_to_text/stt_page.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+
+import '../../main.dart';
 
 class SpeechToTextPage extends StatefulWidget {
   const SpeechToTextPage({Key? key}) : super(key: key);
@@ -8,11 +12,49 @@ class SpeechToTextPage extends StatefulWidget {
 }
 
 class _SpeechToTextPageState extends State<SpeechToTextPage> {
+  bool muteAI = false;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("REMOVE THIS WIDGET"),
+    return  Scaffold(
+        backgroundColor: Colours.backgroundColor,
+        body: SafeArea(
+          child:Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Neumorphic(
+            child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Neumorphic(
+                      child: const SizedBox(
+                        height: 650,
+                        child: Center(
+                          child: Text("Voice will be turned to text"),
+                        ),
+                      ),
+                    ),
+                    NeumorphicButton(
+                      padding: const EdgeInsets.all(10),
+                      child: Icon(
+                        muteAI
+                            ? CupertinoIcons.mic_off
+                            : CupertinoIcons.mic,
+                        size: 30,
+                        color: Colours.tertiaryColor,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          muteAI = !muteAI;
+                        });
+                      },
+                    ),
+                    const SizedBox(
+                      height: 30,
+                      width: double.infinity,
+                    )
+                  ],
+              ),
+          ),
+        ),
       ),
     );
   }
