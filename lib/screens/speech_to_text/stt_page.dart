69a3d398957a68ff95fe1d9960219bfd17f405b3
Meenakshi Nair <meenakshin@firstbytedigital.com>
2022-05-31 13:09:42 +0530

    plugin added

--- lib/screens/speech_to_text/stt_page.dart
+++ lib/screens/speech_to_text/stt_page.dart
@@ -1,5 +1,7 @@
+import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+//import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt1;
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 import '../../main.dart';
@@ -12,50 +14,98 @@ class SpeechToTextPage extends StatefulWidget {
 }
 
 class _SpeechToTextPageState extends State<SpeechToTextPage> {
-  bool muteAI = false;
+  bool muteAI = true;
+  late stt1.SpeechToText _speech;
+  String text = "Please press the microphone button to start speaking";
+
+  @override
+  void initState() {
+    super.initState();
+    _speech = stt1.SpeechToText();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-        backgroundColor: Colours.backgroundColor,
-        body: SafeArea(
-          child:Padding(
+    return Scaffold(
+      backgroundColor: Colours.backgroundColor,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: AvatarGlow(
+        animate: !muteAI,
+        glowColor: Colors.pink,
+        endRadius: 50.0,
+        duration: const Duration(milliseconds: 2000),
+        repeatPauseDuration: const Duration(milliseconds: 100),
+        repeat: true,
+        child: FloatingActionButton(
+          child: Icon(
+            muteAI ? CupertinoIcons.mic_off : CupertinoIcons.mic,
+            size: 30,
+            color: Colors.white54,
+          ),
+          onPressed: () {
+            _listen();
+          },
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
           padding: const EdgeInsets.all(15.0),
           child: Neumorphic(
             child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Neumorphic(
-                      child: const SizedBox(
-                        height: 650,
-                        child: Center(
-                          child: Text("Voice will be turned to text"),
-                        ),
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                SizedBox(
+                  height: 650,
+                  child: Center(
+                    child: Text(
+                      text,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        fontSize: 24,
                       ),
                     ),
-                    NeumorphicButton(
-                      padding: const EdgeInsets.all(10),
-                      child: Icon(
-                        muteAI
-                            ? CupertinoIcons.mic_off
-                            : CupertinoIcons.mic,
-                        size: 30,
-                        color: Colours.tertiaryColor,
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          muteAI = !muteAI;
-                        });
-                      },
-                    ),
-                    const SizedBox(
-                      height: 30,
-                      width: double.infinity,
-                    )
-                  ],
-              ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                  width: double.infinity,
+                )
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  void _listen() async {
+    if (!muteAI) {
+      setState(() {
+        muteAI = true;
+      });
+      _speech.stop();
+    } else {
+      setState(() {
+        text = "Listening...";
+      });
+
+      bool available = await _speech.initialize(
+        onStatus: (val) => print('onStatus: $val'),
+        onError: (val) => print('onError: $val'),
+      );
+
+      if (available) {
+        setState(() {
+          muteAI = false;
+        });
+
+        _speech.listen(
+            onResult: (val) {
+              setState(() {
+                text = val.recognizedWords;
+              });
+            });
+      }
+    }
   }
 }
