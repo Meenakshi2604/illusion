d5c0fbaf7a242342c30b41e060718f557aa614a7
Niranjan B <niranjannb7777@gmail.com>
2022-06-09 07:55:22 +0530

    Commits

--- lib/screens/text_to_speech/tts_page.dart
+++ lib/screens/text_to_speech/tts_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+//import 'package:flutter_tts/flutter_tts.dart';
 
 class TextToSpeechPage extends StatefulWidget {
   const TextToSpeechPage({Key? key}) : super(key: key);
@@ -8,25 +9,52 @@ class TextToSpeechPage extends StatefulWidget {
 }
 
 class _TextToSpeechPageState extends State<TextToSpeechPage> {
+  final _textController = TextEditingController();
+  
+  String userPost = 'Type something and press the play button';
   @override
   Widget build(BuildContext context) {
-    var hintText2 = 'Type Something';
+    
     return Scaffold(
-      body: Column(mainAxisAlignment: MainAxisAlignment.end, children: [
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-        child : TextField(
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            suffixIcon: IconButton(
-            icon: Icon(Icons.send),
-            onPressed: (null),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            //display text
+            Expanded(
+              child: Container(
+                child: Center(
+                child: Text(userPost,textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 35),),
               )
-           // hintText: hintText2,
-            ),
-          ),
+              )),
+
+            //user input text field
+            TextField(
+              controller: _textController,
+              decoration: InputDecoration(
+                //hintText: ('Type something and press the mic'),
+                border: const OutlineInputBorder(),
+              suffixIcon: IconButton(
+                onPressed:()
+                {
+                  setState(() {
+                    userPost = _textController.text;
+                    if (userPost == '') {
+                      userPost = 'Type something and press the play button';
+                    }
+
+                  });
+                },
+                icon: const Icon(Icons.play_arrow_rounded))
+              ),
+            )
+
+          ],
         ),
-      ]),
+      ),
     );
   }
 }
